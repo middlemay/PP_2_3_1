80758efbf1aad198ddbbb6ab598c007e12be45fe
Julia Hortova <lawrova.julia@gail.com>
2022-12-25 20:10:22 +0300

    it's working now

--- src/main/java/web/dao/UserDaoImpl.java
+++ src/main/java/web/dao/UserDaoImpl.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 import web.model.User;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import javax.transaction.Transactional;
@@ -12,17 +13,17 @@ import java.util.List;
 @Transactional
 @Repository
 public class UserDaoImpl implements UserDao{
-    @PersistenceContext(unitName = "entityManager")
-    EntityManager entityManager;
 
-    public UserDaoImpl(EntityManager entityManager) {
-        this.entityManager = entityManager;
+    EntityManagerFactory entityManagerFactory;
+
+    public UserDaoImpl(EntityManagerFactory entityManagerFactory) {
+        this.entityManagerFactory = entityManagerFactory;
     }
 
 
     @Override
     @SuppressWarnings("unchecked")
     public List<User> getUsersList() {
-        return entityManager.createQuery("from User").getResultList();
+        return entityManagerFactory.createEntityManager().createQuery("from User").getResultList();
     }
 }
